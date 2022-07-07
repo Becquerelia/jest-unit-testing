2190bf417cc558cf8af7e2b698701503850b23f5
Eva GC <evarteca@gmail.com>
2022-07-07 09:57:55 +0200

    test palindrome function

--- tests/palindrome.test.js
+++ tests/palindrome.test.js
@@ -1,6 +1,13 @@
 const { palindrome } = require ("../utils/for_testing");
 
-test("palindrome test", () => {
+//! TEST FOR "probando":
+test("palindrome of probando", () => {
     const result = palindrome("probando");
     expect(result).toBe("odnaborp");
-})
+});
+
+//! TEST FOR EMPTY STRING:
+test("palindrome of empty string", () => {
+    const result = palindrome("");
+    expect(result).toBe("");
+});
